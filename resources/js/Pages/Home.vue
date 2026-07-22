3f5f1e4ff08f6b5f828990e8a5253758465a0308
kickymaulana <kickymaulana@gmail.com>
2026-07-22 09:07:30 +0700

    tambahkan lang ts

--- resources/js/Pages/Home.vue
+++ resources/js/Pages/Home.vue
@@ -1,12 +1,15 @@
-<script setup>
-defineProps({
-  user: String
-})
+<script setup lang="ts">
+// Definisikan tipe untuk props
+interface Props {
+  user: string
+}
+
+defineProps<Props>()
 </script>
 
 <template>
-  <div style="padding: 2rem; font-family: sans-serif;">
-    <h1>Halo, {{ user }}! 👋</h1>
-    <p>Inertia v3 + Laravel 13 berhasil terpasang.</p>
+  <div class="p-8 font-sans">
+    <h1 class="text-2xl font-bold text-slate-800">Halo, {{ user }}! 👋</h1>
+    <p class="text-slate-600 mt-2">Inertia v3 + Laravel 13 + TypeScript + Tailwind v4 siap digunakan.</p>
   </div>
 </template>
