d74f13b6132e222e37ed59d1ee455cb931069ae6
kickymaulana <kickymaulana@gmail.com>
2026-07-22 09:17:57 +0700

    ubah style js ke style/index.css di file app.ts

--- resources/js/app.ts
+++ resources/js/app.ts
@@ -1,6 +1,6 @@
 import '../css/app.css'
 import Varlet from '@varlet/ui'
-import '@varlet/ui/es/style.js'
+import '@varlet/ui/es/style/index.css'
 import { createInertiaApp } from '@inertiajs/vue3'
 
 createInertiaApp({
