911ee56513316434209693501a5b85d3aaa7458d
kickymaulana <kickymaulana@gmail.com>
2026-07-22 08:55:49 +0700

    tambahkan halaman testing home.vue

---
+++ resources/js/Pages/Home.vue
@@ -0,0 +1,12 @@
+<script setup>
+defineProps({
+  user: String
+})
+</script>
+
+<template>
+  <div style="padding: 2rem; font-family: sans-serif;">
+    <h1>Halo, {{ user }}! 👋</h1>
+    <p>Inertia v3 + Laravel 13 berhasil terpasang.</p>
+  </div>
+</template>
