42358a6b7af119fa76e6a8026033b31bfa390e78
kickymaulana <kickymaulana@gmail.com>
2026-07-22 09:10:48 +0700

    daftarkan varlet ui ke app.ts dengan withApp

--- resources/js/app.ts
+++ resources/js/app.ts
@@ -1,4 +1,10 @@
 import '../css/app.css'
+import Varlet from '@varlet/ui'
+import '@varlet/ui/es/style'
 import { createInertiaApp } from '@inertiajs/vue3'
 
-createInertiaApp()
+createInertiaApp({
+  withApp(app) {
+    app.use(Varlet)
+  },
+})
